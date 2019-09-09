ad5a2116dfabae58b004c9b24cdffd1bbb4fcd87
siweilxy <siweilxy@163.com>
2019-09-10 05:28:04 +0800

    add log code

--- include/log/log.h
+++ include/log/log.h
@@ -7,9 +7,27 @@
 
 #ifndef INCLUDE_LOG_H_
 #define INCLUDE_LOG_H_
-
-
+#include <string.h>
+#include <string>
+#include <stdarg.h>
+#include <stdio.h>
 int log_init();
 
+#define LOG(format, p,...)  f(format, p,  ##__VA_ARGS__)
+
+void f(std::string p,...)
+{
+    char buf[256] = {0};
+    va_list list;
+    va_start(list, p);
+    vsprintf(buf, p.c_str(), list);
+    printf("%s", buf);
+    va_end(list);
+
+}
+void g(char* str)
+{
+    printf("%s\n",str);
+}
 
 #endif /* INCLUDE_LOG_H_ */
--- src/test/main.cpp
+++ src/test/main.cpp
@@ -4,5 +4,8 @@
 int main()
 {
     log_init();
+    f("%s%s", "good", "boy");
+    LOG("%s%s%d%d","good","boy",1,2);
+
     return 0;
 }
