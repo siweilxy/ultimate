0f4bc454453218ab1bc3c85c5a6c7569d8dc0668
siweilxy <siweilxy@163.com>
2019-09-10 22:35:06 +0800

    add log test

--- src/test/main.cpp
+++ src/test/main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 #include "log.h"
+#include <pthread.h>
+
+void* test(void*)
+{
+    while (1)
+    {
+        DEBUG("%s%s%d%d", "good", "boy", 1, 2);
+        ERROR("%s", "error");
+        WARN("%s", "warn");
+    }
+}
 
 int main()
 {
     log_init();
 
-    DEBUG("%s%s%d%d", "good", "boy", 1, 2);
-    ERROR("%s", "error");
-    WARN("%s", "warn");
+    pthread_t p;
+    pthread_create(&p, NULL, test, NULL);
 
+    while (1)
+    {
+        DEBUG("%s%s%d%d", "good", "boy", 1, 2);
+        ERROR("%s", "error");
+        WARN("%s", "warn");
+    }
     return 0;
 }
